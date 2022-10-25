d798d3a45e87f3af3c4411fd1dfe9db62a91b45c
Lucas Valente <lucas.valente@asksuite.com>
2022-10-25 18:48:42 -0300

    :sparkles: feat: Added all styles and treatments on personal information page

--- src/pages/PersonalInformations/index.tsx
+++ src/pages/PersonalInformations/index.tsx
@@ -1,5 +1,318 @@
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import {
+  FiChevronsRight,
+  FiCornerDownRight,
+  FiHome,
+  FiMail,
+  FiMap,
+  FiMapPin,
+  FiPhone,
+  FiPlus,
+  FiUser,
+  FiUserPlus,
+} from 'react-icons/fi';
+import * as Yup from 'yup';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
+import City from '../../models/City';
+import CountryState from '../../models/CountryState';
+import Person from '../../models/Person';
+import CityService from '../../services/CityService';
+import CountryStateService from '../../services/CountryStateService';
+import PersonService from '../../services/PersonService';
+import ProfessionalService from '../../services/ProfessionalService';
+import UserService from '../../services/UserService';
+import getValidationErrors from '../../utils/getValidationErrors';
+import { Container, InputContainer } from './styles';
+
 const PersonalInformations: React.FC = () => {
-  return <h1>Dados pessoais</h1>;
+  const formRef = useRef<FormHandles>(null);
+
+  const { user, personCpf, refreshToken } = useAuth();
+
+  const [countryStates, setCountryStates] = useState<CountryState[]>([]);
+  const [cities, setCities] = useState<City[]>([]);
+  const [selectedCountryState, setSelectedCountryState] = useState<number>();
+
+  const [currentPerson, setCurrentPerson] = useState<Person | null>();
+
+  const { addToast } = useToast();
+
+  const DEFAULT_VALUE = '';
+
+  useEffect(() => {
+    async function findAllCountryStates() {
+      setCountryStates(await CountryStateService.findAll());
+    }
+
+    findAllCountryStates();
+  }, []);
+
+  useEffect(() => {
+    async function loadData() {
+      const person = personCpf
+        ? await PersonService.findByCpf(personCpf)
+        : null;
+
+      if (person) {
+        const {
+          cpf,
+          name,
+          email,
+          phoneNumber,
+          countryStateId,
+          zipCode,
+          cityId,
+          district,
+          address,
+          complement,
+          userId,
+        } = person;
+
+        setSelectedCountryState(person.countryStateId);
+
+        setCities(await CityService.findByCountryState(person.countryStateId));
+
+        formRef.current?.setData({
+          cpf,
+          name,
+          email,
+          phoneNumber,
+          countryStateId,
+          zipCode,
+          cityId,
+          district,
+          address,
+          complement,
+          userId,
+        });
+      }
+
+      setCurrentPerson(person);
+    }
+
+    loadData();
+  }, [personCpf]);
+
+  const onSuccess = useCallback(() => {
+    addToast({
+      type: 'success',
+      title: 'Sucesso',
+      description: 'Seus dados foram salvos.',
+    });
+
+    if (!personCpf) {
+      refreshToken(formRef.current?.getFieldValue('cpf') as string);
+    }
+  }, [addToast, personCpf, refreshToken]);
+
+  const onError = useCallback(() => {
+    addToast({
+      type: 'error',
+      title: 'Erro',
+      description: 'Ocorreu um erro ao salvar seus dados, tente novamente.',
+    });
+  }, [addToast]);
+
+  const onErrorSessionExpired = useCallback(() => {
+    addToast({
+      type: 'error',
+      title: 'Erro',
+      description: 'Sua sessão expirou, tente novamente.',
+    });
+  }, [addToast]);
+
+  const processInsert = useCallback(
+    async (person: Person) => {
+      const currentUser = await UserService.findUserByUsername(user.username);
+
+      if (!currentUser || !currentUser.id) {
+        onErrorSessionExpired();
+      }
+
+      Object.assign(person, { user: currentUser });
+
+      if (await PersonService.create(person)) {
+        const response = await ProfessionalService.create({
+          personCpf: person.cpf,
+        });
+
+        return response;
+      }
+
+      return false;
+    },
+    [onErrorSessionExpired, user],
+  );
+
+  const handleSubmit = useCallback(
+    async (person: Person) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          cpf: Yup.string()
+            .min(11, 'O CPF precisa conter todos os números')
+            .required('O CPF é obrigatório'),
+          name: Yup.string().required('O nome completo é obrigatório'),
+          email: Yup.string().required('O e-mail é obrigatório'),
+          phoneNumber: Yup.string().required(
+            'O número de telefone é obrigatório',
+          ),
+          countryStateId: Yup.string().required('O estado é obrigatório'),
+          zipCode: Yup.string().required('O CEP é obrigatório'),
+          cityId: Yup.string().required('A cidade é obrigatória'),
+          district: Yup.string().required('O bairro é obrigatório'),
+          address: Yup.string().required('O endereço é obrigatório'),
+          complement: Yup.string(),
+        });
+
+        await schema.validate(person, {
+          abortEarly: false,
+        });
+
+        const response = !currentPerson
+          ? await processInsert(person)
+          : await PersonService.update(person);
+
+        response ? onSuccess() : onError();
+      } catch (err: Yup.ValidationError | any) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        onError();
+      }
+    },
+    [currentPerson, onError, onSuccess, processInsert],
+  );
+
+  const getTitle = useCallback(() => {
+    return `Bem vindo ${currentPerson ? currentPerson.name : user.username}`;
+  }, [user, currentPerson]);
+
+  const getSubtitle = useCallback(() => {
+    return !personCpf
+      ? 'Continue seu cadastro de dados pessoais'
+      : 'Você pode editar seus dados aqui quando quiser';
+  }, [personCpf]);
+
+  const onChangeCountryState = async (countryState: number) => {
+    setSelectedCountryState(countryState);
+
+    setCities(await CityService.findByCountryState(countryState));
+  };
+
+  return (
+    <Container>
+      <h1>{getTitle()}</h1>
+      <h3>{getSubtitle()}</h3>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <Input
+          name="name"
+          maxLength={250}
+          icon={FiUser}
+          placeholder="Nome completo"
+        />
+
+        <Input
+          name="cpf"
+          maxLength={11}
+          icon={FiUserPlus}
+          placeholder="CPF"
+          isDisabled={!!currentPerson?.cpf}
+        />
+
+        <Input
+          name="email"
+          maxLength={250}
+          icon={FiMail}
+          placeholder="E-mail"
+        />
+
+        <InputContainer>
+          <Input
+            name="phoneNumber"
+            icon={FiPhone}
+            type="tel"
+            placeholder="Telefone"
+          />
+
+          <Input name="zipCode" icon={FiChevronsRight} placeholder="CEP" />
+        </InputContainer>
+
+        <Select
+          name="countryStateId"
+          icon={FiMap}
+          placeholder="Estado"
+          defaultValue={DEFAULT_VALUE}
+          onChange={onChangeCountryState}
+        >
+          <option value={DEFAULT_VALUE} disabled>
+            Selecione um estado. . .
+          </option>
+
+          {countryStates.map(countryState => (
+            <option key={countryState.id} value={countryState.id}>
+              {countryState.uf} - {countryState.name}
+            </option>
+          ))}
+        </Select>
+
+        <Select
+          name="cityId"
+          icon={FiMapPin}
+          placeholder="Cidade"
+          defaultValue={DEFAULT_VALUE}
+          isDisabled={!selectedCountryState}
+        >
+          <option value={DEFAULT_VALUE} disabled>
+            Selecione uma cidade. . .
+          </option>
+
+          {cities.map(city => (
+            <option key={city.id} value={city.id}>
+              {city.name}
+            </option>
+          ))}
+        </Select>
+
+        <Input
+          name="address"
+          maxLength={250}
+          icon={FiHome}
+          placeholder="Endereço"
+        />
+
+        <InputContainer>
+          <Input
+            name="district"
+            maxLength={100}
+            icon={FiCornerDownRight}
+            placeholder="Bairro"
+          />
+
+          <Input
+            name="complement"
+            maxLength={250}
+            icon={FiPlus}
+            placeholder="Complemento"
+          />
+        </InputContainer>
+
+        <Button type="submit">Salvar</Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default PersonalInformations;
---
+++ src/pages/PersonalInformations/styles.ts
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: top;
+  align-items: center;
+  flex-direction: column;
+  padding: 40px;
+
+  h3 {
+    margin-bottom: 24px;
+    font-size: 14px;
+    font-weight: 400;
+  }
+
+  form {
+    margin: 40px 0;
+    width: 80%;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0ms.2s;
+
+      &:hover {
+        color: shade(0.2, '#f4ede8');
+      }
+    }
+
+    button {
+      max-width: 180px;
+      align-self: flex-end;
+      background: #56be7f;
+      font-weight: 800;
+
+      &:hover {
+        background: #4dab72;
+      }
+    }
+  }
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+
+  div,
+  input,
+  option {
+    min-width: 50px;
+    margin-top: 0;
+
+    + div {
+      margin-left: 10px;
+    }
+  }
+`;
