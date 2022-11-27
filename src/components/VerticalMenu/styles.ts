96a8a088f45391067db8275133e2aa11fb71da02
Lucas Valente <lucas.valente9999@gmail.com>
2022-11-27 17:45:03 -0300

    :lipstick: feat: Fixed vertical menu style

--- src/components/VerticalMenu/styles.ts
+++ src/components/VerticalMenu/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { shade } from 'polished';
 
 export const Container = styled.nav`
-  height: 100vh;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
